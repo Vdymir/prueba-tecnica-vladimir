c81d73df0e112ab08fdc5fca6c6458b12e865216
vdymir <vladistar200302@gmail.com>
2024-11-18 10:53:46 -0500

    fix: adjustment

--- src/constant/list-poke-balls.ts
+++ src/constant/list-poke-balls.ts
@@ -11,22 +11,22 @@ export const listWithSixPokeBalls = [
   },
   {
     className:
-      "absolute top-[-50px] left-[-50px] lg:top-0 lg:left-0 opacity-20 h-52 w-52",
+      "absolute top-[-50px] left-[-50px] lg:top-0 lg:left-0 opacity-20 w-52",
     id: 3,
   },
   {
     className:
-      "absolute bottom-[200px] right-[-250px] lg:bottom-[-150px] lg:right-[-100px] opacity-20 h-96 w-96",
+      "absolute bottom-[200px] right-[-250px] lg:bottom-[-150px] lg:right-[-100px] opacity-20 w-96",
     id: 4,
   },
   {
     className:
-      "absolute hidden lg:flex lg:top-[400px] lg:left-[400px] opacity-20 h-52 w-52",
+      "absolute hidden lg:flex lg:top-[400px] lg:left-[400px] opacity-20 w-52",
     id: 5,
   },
   {
     className:
-      "absolute top-44 lg:bottom-[50px] lg:right-[400px] opacity-20 h-52 w-52",
+      "absolute top-44 lg:bottom-[50px] lg:right-[400px] opacity-20 w-52",
     id: 6,
   },
 ];
@@ -34,23 +34,23 @@ export const listWithSixPokeBalls = [
 export const listWithThreePokeBalls = [
   {
     className:
-      "absolute top-[-20px] right-[-100px] lg:top-20 lg:right-[-100px] opacity-10 h-60 w-60",
+      "absolute top-[-20px] right-[-100px] lg:top-20 lg:right-[-100px] opacity-10  w-60",
     id: 1,
   },
   {
     className:
-      "absolute bottom-[-40px] left-[-80px] lg:left-[-40px] opacity-10 h-32 w-32",
+      "absolute bottom-[-40px] left-[-80px] lg:left-[-40px] opacity-10 w-32",
     id: 2,
   },
   {
-    className: "absolute top-[-40px] left-[-40px] opacity-10 h-32 w-32",
+    className: "absolute top-[-40px] left-[-40px] opacity-10 w-32",
     id: 3,
   },
 ];
 export const listWithFourPokeBalls = [
   {
     className:
-      "absolute top-0 right-0 lg:top-[-100px] lg:right-[-50px] opacity-35 h-32 w-32",
+      "absolute top-0 right-0 lg:top-[-100px] lg:right-[-50px] opacity-35 w-32 lg:w-96",
     id: 1,
   },
   {
@@ -60,12 +60,12 @@ export const listWithFourPokeBalls = [
   },
   {
     className:
-      "absolute top-[-30px] left-[-60px] lg:top-24 lg:left-0 opacity-35 h-52 w-52",
+      "absolute top-[-30px] left-[-60px] lg:top-24 lg:left-0 opacity-35 w-52",
     id: 3,
   },
   {
     className:
-      "absolute right-[-100px] lg:bottom-24 lg:right-[250px] opacity-35 h-52 w-52",
+      "absolute right-[-100px] lg:bottom-24 lg:right-[250px] opacity-35 w-52",
     id: 4,
   },
 ];
